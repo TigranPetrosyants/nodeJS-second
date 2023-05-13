7ef1bfc59246c24812734d1a7394763f91e5802b
TigranPetrosyants <tigran.petrosyants.fd@gmail.com>
2023-05-13 16:12:26 +0400

    Add POST method with json body parser

--- books.js
+++ books.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const { MongoClient } = require('mongodb');
 
@@ -16,12 +17,32 @@ async function main() {
 
   const collection = db.collection('books');
 
+  const findResult = await collection.find({}).toArray();
+
   const app = express();
+
+  app.use(bodyParser.json());
 
   app.get('/', async (req, res) => {
     const books = await collection.find().toArray();
     res.status(200).send(books);
   })
+
+  app.post('/', (req, res) => {
+    if (req.body.title && req.body.author) {
+        const {title, author} = req.body;
+        const newBook = {
+            date: new Date(),
+            title,
+            author,
+        }
+        collection.insertOne(newBook);
+        res.status(201).send(newBook);
+    }else {
+        res.status(404).send('The Title or Author fields are empty.');
+    }
+  })
+
 
   app.listen(PORT, () => {
     console.log('Server running on 5000 port');
